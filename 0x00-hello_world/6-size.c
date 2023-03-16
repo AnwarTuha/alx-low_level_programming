5f95480f77c18d10467a69569a797a77d6802ec1
AnwarTuha <anwartuha2@gmail.com>
2023-03-16 11:20:59 -0700

    fixig for betty compliance

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,11 @@
  *  Return: success denoted by 0
  *
  */
-int main(void){
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-		
-	printf("Size of a char: %2d byte(s)\n", sizeof(d));
-	printf("Size of an int: %2d byte(s)\n", sizeof(a));
-	printf("Size of a long int: %2d byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %2d byte(s)\n", sizeof(c));
-	printf("Size of a float: %2d byte(s)\n", sizeof(f));
+int main(void){	
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)\n", sizeof(float));
   	return 0;
 }

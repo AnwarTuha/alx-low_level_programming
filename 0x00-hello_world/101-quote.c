9e8299e68a92f50f35e05df2e3deb7dd455cbb5d
AnwarTuha <anwartuha2@gmail.com>
2023-03-16 11:40:06 -0700

    script that prints quote from task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *  main - prints quote from task 8 in 0x00. C - Hello, World
+ *
+ *  Return: success denoted by 1
+ *
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

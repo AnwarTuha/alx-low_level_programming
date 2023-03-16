f2a1da00d509b5758b2990c4c3fd8e99991c0e78
AnwarTuha <anwartuha2@gmail.com>
2023-03-16 11:46:42 -0700

    fixing for betty compliance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *  main - prints quote from task 8 in 0x00. C - Hello, World
@@ -9,6 +10,6 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }

43ba55b04e24258bbe347142300b1bf699254879
AnwarTuha <anwartuha2@gmail.com>
2023-03-16 10:40:35 -0700

    added betty compliance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
 #include <string.h>
-
-int main(void){
-  
-  puts("Programming is like building a multilingual puzzle\n");
-  	
-  return (0);
+#include <stdio.h>
+/**
+ *  main - Entry, prints quote with puts
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }

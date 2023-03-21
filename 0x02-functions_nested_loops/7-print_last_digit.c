2085b2fd312264eee67e493b66f7358dcdf2cbaa
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 07:14:26 -0700

    program that prints last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints last digit of input
+ *
+ * @n: the integer value it receives
+ *
+ * Return: integer, last digit of input
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

7b4c3be6ad6e5009b49b22e78facb17a760939bf
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 11:32:05 -0700

    program that lists the first 50 fibonnaci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Lists the first 50 fibonnacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	int i;
+
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 48; i++)
+	{
+		unsigned long int next = a + b;
+
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+
+	return (0);
+}

54a95983da59ac343bcdfb14d2726ac0a1be0f35
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 05:39:55 -0700

    script that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Entry point, prints "_putchar"
+ *
+ *  Return: success denoted by 0
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}

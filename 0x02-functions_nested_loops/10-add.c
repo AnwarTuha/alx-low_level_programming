a50231dd77b85bd430a9efea13deba5a4e6e2534
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 10:25:43 -0700

    program that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints last digit of input
+ *
+ * @n: the integer value it receives
+ * @m: the integer value it receives
+ *
+ * Return: integer, last digit of input
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}

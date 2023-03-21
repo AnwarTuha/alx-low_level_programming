991760605007acb11f8c8e0d665f44a396e28b0e
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 07:10:21 -0700

    program that prints absolute value of input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - prints absolute value of input
+ *
+ * @n: the integer value it receives
+ *
+ * Return: integer, absolute value of input
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}

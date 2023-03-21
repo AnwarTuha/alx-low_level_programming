9b8f838adbc76fa7e18bfc9a64b10a44d7bbd20e
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 05:56:36 -0700

    added head file containg _putchar prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  *  main - Entry point, prints "_putchar"
@@ -10,7 +8,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }

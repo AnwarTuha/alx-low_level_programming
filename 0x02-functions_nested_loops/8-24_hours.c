c3190034ca34114c9c2106e14a50fbed99774e6c
AnwarTuha <anwartuha2@gmail.com>
2023-03-21 07:38:47 -0700

    program that prints hours and minutes in 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every hour and minute in 24 hours
+ *
+ */
+void jack_bauer(void)
+{
+	for (int hour = 0; hour < 24; hour++)
+	{
+		for (int minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
+}

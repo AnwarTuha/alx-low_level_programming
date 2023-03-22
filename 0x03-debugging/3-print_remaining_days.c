127a194b5ec8ca68389a7812244f8f2ab434d97c
AnwarTuha <anwartuha2@gmail.com>
2023-03-22 16:52:57 +0300

    fix for ISO C90 compliance

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,14 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	int i;
 	int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		month_days[1] = 29;
 	}
-
-	int i;
 
 	for (i = 0; i < month - 1; i++)
 	{

536dac361b1fb0e893754d69542036d1fb8c3be3
AnwarTuha <anwartuha2@gmail.com>
2023-03-23 14:41:08 +0300

    program that prints the fizzbuzz sequence

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* main - prints the fizzbuzz sequence
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
+

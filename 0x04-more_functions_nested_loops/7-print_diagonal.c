7746af568edd658a726873692d8bc9b1de2de44b
AnwarTuha <anwartuha2@gmail.com>
2023-03-23 14:21:12 +0300

    program that prints '\' n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* print_diagonal - prints '\' n times
+*
+* @n: integer that indicates number of '\'
+*
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+    if (n <= 0)
+    {
+        putchar('\n');
+    }
+    else
+    {
+        int i;
+
+        for (i = 0; i < n; i++)
+        {
+            putchar(' ');
+        }
+        putchar('\\');
+        putchar('\n');
+    }
+}

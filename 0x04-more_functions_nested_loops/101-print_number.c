1b7df20cd1ce7a45ce2e69257b354ccb909cca9d
AnwarTuha <anwartuha2@gmail.com>
2023-03-23 16:16:15 +0300

    program that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,7 +14,7 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n / 10 != 0)
@@ -22,5 +22,5 @@ void print_number(int n)
 		print_number(n / 10);
 	}
 
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }

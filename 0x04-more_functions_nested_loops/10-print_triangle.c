6e6cb2c26c3963dc89e1c83059703e3c3924f27b
AnwarTuha <anwartuha2@gmail.com>
2023-03-23 15:23:50 +0300

    * -> # lol

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,17 +15,24 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= (size - i); j++)
-		{
-			putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			putchar('#');
-		}
 		putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= (size - i); j++)
+			{
+				putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
 	}
 }
 

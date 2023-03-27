77acc19537cfe3cfdcdea4cc646451fe92cf69b8
AnwarTuha <anwartuha2@gmail.com>
2023-03-27 15:36:48 +0300

    function that prints given string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _puts - prints given string
+*
+* @str: string to be printed
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}

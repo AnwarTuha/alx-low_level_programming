b2194c9d94792c570098a2cf8b89bcb91d2ba414
AnwarTuha <anwartuha2@gmail.com>
2023-03-27 21:15:03 +0300

    function that prints a number of elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_array - prints half of a given string
+*
+* @a: array of numbers
+* @n: number of elements to be printed
+*
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+
+		if (i < ((n - 1)))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}

4df8bf42669399686ce17990a76c9b30270aff60
AnwarTuha <anwartuha2@gmail.com>
2023-03-27 14:58:03 +0300

    program that resets value of a pointers pointed variable to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* reset_to_98 - resets value of *n to 98
+*
+* @*n: integer that indicates number of '-'
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

f767aa68a78eae8f9a3fda5e44bd6aff6242ec9d
AnwarTuha <anwartuha2@gmail.com>
2023-03-29 09:42:30 +0300

    function that compares two given strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcmp - copies one string to another
+*
+* @s1: destination string to be copied to
+* @s2: source string to copy from
+*
+* Return: copied string from src string
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

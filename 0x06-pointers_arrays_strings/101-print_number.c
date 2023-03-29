b6cdde824967cb5ff4329ac92f7d01d950685365
AnwarTuha <anwartuha2@gmail.com>
2023-03-29 21:17:33 +0300

    function that prints given integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_number - prints out given integer
+*
+* @n: integer to be printed
+*
+* Return: void
+*/
+
+void print_number(int n)
+{
+	int divisor = 1;
+	int num = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+
+	while (num / divisor > 9)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+		putchar('0' + num / divisor);
+		num %= divisor;
+		divisor /= 10;
+	}
+}

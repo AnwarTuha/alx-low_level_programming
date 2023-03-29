bb4a1d8f7f0756c61d9c25f5faa3cec728f30c4e
AnwarTuha <anwartuha2@gmail.com>
2023-03-29 21:04:09 +0300

    function that encodes string in rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* rot13 - encodes given string in rot13.
+*
+* @s: string to be encoded
+*
+* Return: encoded string
+*/
+
+char *rot13(char *s)
+{
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	char *current_char;
+
+	char *result = malloc(strlen(s));
+
+	if (result != NULL)
+	{
+		strcpy(result, s);
+		current_char = result;
+
+		while (*current_char != '\0')
+		{
+			if ((*current_char >= 97 && *current_char <= 122) ||
+				(*current_char >= 65 && *current_char <= 90))
+			{
+				if (*current_char > 109 || (*current_char > 77 && *current_char < 91))
+				{
+					*current_char -= 13;
+				}
+				else
+				{
+					*current_char += 13;
+				}
+			}
+			current_char++;
+		}
+	}
+
+	return (result);
+}

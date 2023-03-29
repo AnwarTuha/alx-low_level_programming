101205b9db6f4f6d29b4ccd134992be239bd81d0
AnwarTuha <anwartuha2@gmail.com>
2023-03-29 19:46:46 +0300

    minor fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -22,16 +22,4 @@ void reverse_array(int *a, int n)
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
 	}
-
-	for (i = 0; i < n; i++)
-	{
-		if (i != n - 1)
-		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
-			printf("%d", a[i]);
-		}
-	}
 }

cda000ec94d78fde7f8b42418908594de0afdbda
AnwarTuha <anwartuha2@gmail.com>
2023-04-03 08:55:26 +0300

    function that sets n bytes of data b to memory s

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _memset - capitalizes all characters of given string
+*
+* @s: memory (address) to be filled
+* @b: the data to be filled
+* @n: the first bytes of memory to be filled
+*
+* Return: converted string
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(n, s, b);
+
+	return (s);
+}

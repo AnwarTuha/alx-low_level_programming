f873cb84c1ceacfadc2d097999556e1dd6fa0cbf
AnwarTuha <anwartuha2@gmail.com>
2023-04-07 16:22:49 +0300

    program that multiplies given numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entree point, prints out all arguments of program
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0, denoting success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+	int j;
+	int product;
+	char errorMessage[] = "Error";
+
+	product = 1;
+
+	if (argc == 0)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			putchar(errorMessage[i]);
+		}
+		return (1);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			product = product * argv[i][j];
+			j++;
+		}
+	}
+
+	putchar(48 + product);
+	putchar('\n');
+
+	return (0);
+}

f36e9fa7a171b8f7125a800377d9f3ebf9577821
AnwarTuha <anwartuha2@gmail.com>
2023-04-07 17:04:56 +0300

    function that adds and displays given arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/**
+* print_number - prints given number
+*
+* @n: number to be printed
+*
+* Return: void
+*/
+
+void print_number(int n)
+{
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
+
+/**
+* isNumber - checks whether given number is digit or not
+*
+* @number: number to be checked
+*
+* Return: true if number, false if not
+*/
+
+bool isNumber(char number[])
+{
+	int i = 0;
+
+	for (; number[i] != 0; i++)
+	{
+		if (!isdigit(number[i]))
+		{
+			return (false);
+		}
+	}
+	return (true);
+}
+
+/**
+* main - Entree point, adds and displays given arguments
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0, denoting success
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		putchar('0');
+		putchar('\n');
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		if (!isNumber(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	print_number(sum);
+	putchar('\n');
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int _putchar(char c);
 void print_number(int n);
+bool isNumber(char number[]);
 
 #endif /* MAIN_H */

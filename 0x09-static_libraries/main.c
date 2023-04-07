e7d1eced52b309cc71936a27482801e05e127dc3
AnwarTuha <anwartuha2@gmail.com>
2023-04-07 20:15:46 +0300

    minor fix

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - Entree point, uses puts function to print string
+*
+* Return: 0, denoting success
+*/
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}

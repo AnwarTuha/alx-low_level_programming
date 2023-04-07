b71cf5b54c92429e0c901e39fec7b5c760c25063
AnwarTuha <anwartuha2@gmail.com>
2023-04-07 15:24:50 +0300

    minor fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -17,10 +17,10 @@ int main(int argc, char *argv[])
 
 	while (argv[0][i] != '\0')
 	{
-		putchar(argv[0][i]);
+		_putchar(argv[0][i]);
 		i++;
 	}
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }

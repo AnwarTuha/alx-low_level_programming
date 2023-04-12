eb4c5ccd8877785932f5bea62fdd7ac6c28d1168
AnwarTuha <anwartuha2@gmail.com>
2023-04-12 14:40:47 +0300

    program that concatinates given strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,42 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	strncat(s1, s2, n);
+	char *new_str;
+	unsigned int len1 = 0;
+	unsigned int len2 = 2;
 
-	return (s1);
+	if (s1 != NULL)
+	{
+		len1 = strlen(s1);
+	}
+	if (s2 != NULL)
+	{
+		len2 = strlen(s2);
+	}
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (s1 != NULL)
+	{
+		strcpy(new_str, s1);
+	}
+	if (s2 != NULL)
+	{
+		strncpy(new_str + len1, s2, n);
+	}
+
+	new_str[len1 + n] = '\0';
+
+	return (new_str);
 }
+

f6e5390c521aa0ee9a47d0bb1b765c9703b19153
AnwarTuha <anwartuha2@gmail.com>
2023-04-12 15:33:01 +0300

    program that multplies given arguments

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_number - prints given number
+*
+* @n: number to be printed
+*
+* Return: void
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
+
+
+/**
+* main - Entree point, multiplies given arguments
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0, denoting success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 3)
+	{
+		putchar('E');
+		putchar('r');
+		putchar('r');
+		putchar('o');
+		putchar('r');
+		putchar('\n');
+
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	print_number(result);
+	putchar('\n');
+
+	return (0);
+}

90cb0c8423f83d35701fbf8e7414ec0e5a1651e6
AnwarTuha <anwartuha2@gmail.com>
2023-04-12 14:20:42 +0300

    added _putchar prototype

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* string_nconcat - concatinates two strings
+*
+* @s1: string to be concatinated
+* @s2: string to be concatinated
+* @n: first n bytes of s2 to be concatinated
+*
+* Return: pointer to the new string
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	strncat(s1, s2, n);
+
+	return(s1);
+}

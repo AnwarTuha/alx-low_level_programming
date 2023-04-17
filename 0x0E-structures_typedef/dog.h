5ef2d4136c7152517ca81e58a60c334bd3917950
AnwarTuha <anwartuha2@gmail.com>
2023-04-17 12:18:24 +0300

    added dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+
+#include <stdio.h>
+
+/**
+ * struct dog - struct of an ideal dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG */

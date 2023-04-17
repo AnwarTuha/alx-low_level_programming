5d682543ac36325e724143bdde0bd87140196e6a
AnwarTuha <anwartuha2@gmail.com>
2023-04-17 12:41:03 +0300

    function that initializes dof struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+ *  init_dog - Entry point, intitializes dog struct
+ *
+ *	@d: pointer to dog struct
+ *	@name: name of dog
+ *	@age: age of dog
+ *	@owner: owner of dog
+ *
+ *  Return: void
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

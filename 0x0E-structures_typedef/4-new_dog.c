fd37fee1500be5f61de6027bced929d679bee278
AnwarTuha <anwartuha2@gmail.com>
2023-04-17 16:55:57 +0300

    betty compliance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,7 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-    strcpy(new_dog->name, name);
+	strcpy(new_dog->name, name);
 
 	new_dog->age = age;
 
@@ -43,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-    strcpy(new_dog->owner, owner);
+	strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }

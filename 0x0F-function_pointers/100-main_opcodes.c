e23ee7f92b0b9ab96c484ec1c9fda957731bf14f
AnwarTuha <anwartuha2@gmail.com>
2023-04-19 16:05:38 +0300

    function that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argc: arguments count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 or 2 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, n;
+	unsigned char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	p = (unsigned char *)main;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", p[i]);
+		if (i < n - 1)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}

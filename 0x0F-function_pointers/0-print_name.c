a3848a9d1befe6ce0c5329d662fa2ddc3cbde4ad
AnwarTuha <anwartuha2@gmail.com>
2023-04-19 12:58:57 +0300

    function that prints given name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  print_name - write a function that prints given name
+ *
+ *	@name: string to be printed
+ *	@f: modifier function for the name
+ *
+ *  Return: void
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

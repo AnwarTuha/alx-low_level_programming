1cb7019a279557b2d02acf927f8ebd20b3ff32df
AnwarTuha <anwartuha2@gmail.com>
2023-04-19 14:02:28 +0300

    created get_op_func file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ *  get_op_func - create new dog from given parameters
+ *
+ *	@name: name of new dog
+ *	@age: age of new dog
+ *	@owner: owner of new dog
+ *
+ *  Return: new dog struct of type dog_t
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+}

ad6853c31e1e2b4697e9d466592d65f5772a75a7
AnwarTuha <anwartuha2@gmail.com>
2023-04-20 21:25:30 +0300

    function that prints all arguments using their respective formats

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *  print_all - prints given arguments with given format
+ *
+ *	@format: format of each argument
+ *
+ *  Return: void
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *s;
+	char *separator = "";
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%s%c", separator, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", separator, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", separator, va_arg(args, double));
+			break;
+		case 's':
+			s = va_arg(args, char *);
+			if (s != NULL)
+				printf("%s%s", separator, s);
+			else
+				printf("(nil)");
+			break;
+
+		default:
+			break;
+		}
+
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}

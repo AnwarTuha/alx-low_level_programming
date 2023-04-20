197e552021dd602ed3701c51de9fc9473f6eafee
AnwarTuha <anwartuha2@gmail.com>
2023-04-20 21:33:43 +0300

    removed else statement

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,10 +36,11 @@ void print_all(const char * const format, ...)
 			break;
 		case 's':
 			s = va_arg(args, char *);
-			if (s != NULL)
-				printf("%s%s", separator, s);
-			else
-				printf("(nil)");
+			if (s == NULL)
+			{
+				s = "(nil)";
+			}
+			printf("%s%s", separator, s);
 			break;
 
 		default:

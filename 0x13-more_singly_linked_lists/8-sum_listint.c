d3c597137dcd824e60877c814e31683cd26214f7
AnwarTuha <anwartuha2@gmail.com>
2023-05-02 17:05:14 +0300

    program that sums n data of listint_t and returns the sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ *  sum_listint - gets the required node from a given linked list
+ *
+ *	@head: pointer to the current head of a linked list
+ *	@index: index of the location of the node
+ *
+ *  Return: required node of listint_t
+ *
+ */
+int sum_listint(listint_t *head)
+{
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+    return (sum);
+}

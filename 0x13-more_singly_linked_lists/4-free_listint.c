9c39b6691b17a4456fe002134b35b501a15a435f
AnwarTuha <anwartuha2@gmail.com>
2023-05-02 16:25:06 +0300

    program that frees a given linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ *  free_listint - frees a given linked list
+ *
+ *	@head: pointer to the current head of a linked list
+ *
+ *  Return: void
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->n);
+		free(temp);
+	}
+}

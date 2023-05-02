54381fefe2711d3078a0465ea7b371367fa1a2f0
AnwarTuha <anwartuha2@gmail.com>
2023-05-02 15:59:34 +0300

    program that prints the elements of a given linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ *  print_listint - prints given arguments with given format
+ *
+ *	@h: linked list where elements to be printed are found
+ *
+ *  Return: size of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->n == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d]\n", h->n);
+		}
+		node_count++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
+	}
+
+	return (node_count++);
+}
